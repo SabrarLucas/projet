6348231ad1c0b365b3da59425dcdcd182b0fd698
SabrarLucas <pinchonlucas03@gmail.com>
2023-11-22 11:51:06 +0100

    restriction de pages avec les voters

---
+++ src/Controller/Admin/ProductsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Products;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/produits', name: 'admin_products_')]
+class ProductsController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('admin/users/index.html.twig');
+    }
+
+    #[Route('/ajout', name: 'add')]
+    public function add(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        return $this->render('admin/users/index.html.twig');
+    }
+
+    #[Route('/edition/{id}', name: 'edit')]
+    public function edit(Products $product): Response
+    {
+        $this->denyAccessUnlessGranted('PRODUCT_EDIT', $product);
+
+        return $this->render('admin/users/index.html.twig');
+    }
+
+    #[Route('/suppression/{id}', name: 'delete')]
+    public function delete(Products $product): Response
+    {
+        $this->denyAccessUnlessGranted('PRODUCT_DELETE', $product);
+        
+        return $this->render('admin/users/index.html.twig');
+    }
+}
